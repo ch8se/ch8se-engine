7acf7f72eeddb64f0238d194a10e302a08f77a65
Radomir Perisic <radomir.perisic@stanga.net>
2015-11-13 16:12:01 +0100

    Footer

--- wp-content/themes/ch8se/footer.php
+++ wp-content/themes/ch8se/footer.php
@@ -1,9 +1,29 @@
 
   </div><!-- .site-content -->
 
-  <footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="site-info">
-    </div><!-- .site-info -->
+  <footer class="site-footer" role="contentinfo">
+
+    <div class="social">
+        <p>If you share our vision,<br>follow us</p>
+        <a href=""><i class="fa fa-facebook"></i></a>
+        <a href=""><i class="fa fa-instagram"></i></a>
+        <a href=""><i class="fa fa-twitter"></i></a>
+    </div>
+
+
+    <nav class="footer-nav">
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Shop</a></li>
+        <li><a href="">Shipping and return</a></li>
+        <li><a href="">Privacy</a></li>
+        <li><a href="">Contact us</a></li>
+        <li><a href="">FAQ</a></li>
+        <li><a href="">ch8seday</a></li>
+        <li><a href="">Ambassadors</a></li>
+        <li><a href="">Impact</a></li>
+      </ul>
+    </nav>
   </footer><!-- .site-footer -->
 
 </div><!-- .site -->
