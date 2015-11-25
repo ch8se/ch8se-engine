56c21ac4903b74d8744ef805823b3e97c88db5f2
Radomir Perisic <radeperisic@yahoo.com>
2015-11-25 22:26:58 +0100

    Impact page

--- wp-content/themes/ch8se/page.php
+++ wp-content/themes/ch8se/page.php
@@ -11,7 +11,7 @@
 get_header(); ?>
 
 
-<div class="content">
+<div class="page-content">
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
       <?php the_content(); ?>
