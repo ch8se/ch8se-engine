ef6cb808f532b009efa75f39d821c288e3f722a1
Radomir Perisic <radeperisic@yahoo.com>
2015-12-09 22:42:26 +0100

    fixes

--- wp-content/themes/ch8se/js/app.jsx
+++ wp-content/themes/ch8se/js/app.jsx
@@ -47,7 +47,7 @@ ch8se.init = function() {
     e.preventDefault();
 
     if ($(e.target).hasClass('reports')) {
-      $('.subscribe-overlay').find('p').hide();
+      $('.subscribe-overlay').find('.float-block > p').hide();
       $('.subscribe-overlay').find('p.reports').show();
     }
     $('.subscribe-overlay').show();
