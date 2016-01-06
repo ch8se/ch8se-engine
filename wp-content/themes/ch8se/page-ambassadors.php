4992b4c9ce6d2adf5a40c40228c2ed943888547b
Radomir Perisic <radeperisic@yahoo.com>
2016-01-06 20:10:21 +0100

    ambassador template files

---
+++ wp-content/themes/ch8se/page-ambassadors.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Products
+ */
+
+get_header(); ?>
+
+<div class="ambassadors">
+  
+  
+</div>
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/ch8se/single-ambassador.php
@@ -0,0 +1,11 @@
+<?php
+
+get_header(); ?>
+
+
+<div class="content">
+  
+</div>
+
+
+<?php get_footer(); ?>
