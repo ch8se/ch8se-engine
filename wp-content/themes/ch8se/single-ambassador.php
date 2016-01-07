673cfe75d5df3c5385b5e3c024a4477add5beaf7
Dusan Djukic <djukdus@gmail.com>
2016-01-07 18:57:30 +0100

    responsive, and small fixes

--- wp-content/themes/ch8se/single-ambassador.php
+++ wp-content/themes/ch8se/single-ambassador.php
@@ -3,14 +3,14 @@
 get_header(); ?>
 
 
-<div class="content">
+<div class="content ambassadors">
   <div class="ambassadors banner">
   	<div class="ambassador image">
   		<img src="wp-content/themes/ch8se/img/demo/ambanner.jpg">
   	</div>
   </div>
-  <div class="bio">
-  	<div class="pic">
+  <div class="biography">
+  	<div class="profile-picture">
   		<img src="wp-content/themes/ch8se/img/demo/ambassador.jpg">
   		<div class="social invert sing">
   			<a href="">
@@ -31,7 +31,7 @@ get_header(); ?>
   		<h2>Steve House</h2>
   		<p>HOMETOWN</p>
   		<p>Beograd, Srbija</p>
-  		<p class="detail">Reinhold Messner called Steve House “the best high-altitude climber in the 
+  		<p class="details">Reinhold Messner called Steve House “the best high-altitude climber in the 
   		 world today.” But for Steve, climbing is about process, not achievement. He says, “The moment
   		 your mind wanders from the climbing at hand will be the moment you fall.” Steve has earned
   		 Messner’s praise by leading the charge to climb bigger routes, from the Himalaya's to the 
