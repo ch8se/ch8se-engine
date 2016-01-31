3ec309fdfbae8c46d65986ca4bf03b6e3cab4934
Radomir Perisic <radeperisic@yahoo.com>
2016-01-31 16:14:37 +0100

    removed subscribe

--- wp-content/themes/ch8se/header.php
+++ wp-content/themes/ch8se/header.php
@@ -86,7 +86,6 @@
         <li>
           <a href="<?php echo esc_url( home_url( '/about-us' ) ); ?>">About us</a>
         </li>
-        <li class="subscribe"><a href="http://www.ch8se.com/share-and-plant/">Plant a tree for free</a></li>
       </ul>
     </nav>
   </header><!-- .site-header -->
