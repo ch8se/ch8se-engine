f6a0a7283a1389d54648bb0357cd2ebb5db18375
Dusan Djukic <djukdus@gmail.com>
2016-02-04 17:12:39 +0100

    blog page

--- wp-content/themes/ch8se/page-blog.php
+++ wp-content/themes/ch8se/page-blog.php
@@ -5,7 +5,70 @@
 
 get_header(); ?>
 
-
+	<div class="blog-wrapper">
+		<div class="blogs">
+			<div class="blog-post">
+				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				  eu fugiat nulla pariatur.</p>
+				  <a href="">Lorem - posted an hour ago</a>
+			</div>
+			<div class="blog-post border">
+				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				  eu fugiat nulla pariatur.</p>
+				  <a href="">Lorem - posted an hour ago</a>
+			</div>
+			<div class="blog-post">
+				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				  eu fugiat nulla pariatur.</p>
+				  <a href="">Lorem - posted an hour ago</a>
+			</div>
+			<div class="blog-post">
+				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				  eu fugiat nulla pariatur.</p>
+				  <a href="">Lorem - posted an hour ago</a>
+			</div>
+			<div class="blog-post border">
+				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				  eu fugiat nulla pariatur.</p>
+				  <a href="">Lorem - posted an hour ago</a>
+			</div>
+			<div class="blog-post">
+				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				  eu fugiat nulla pariatur.</p>
+				  <a href="">Lorem - posted an hour ago</a>
+			</div>
+		</div>
+	</div>
 
 
 
