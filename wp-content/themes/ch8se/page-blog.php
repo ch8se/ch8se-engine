ccafdb838541129613f2abbcc8b3805a18cc7f7b
Dusan Djukic <djukdus@gmail.com>
2016-02-06 17:45:42 +0100

    small fixes

--- wp-content/themes/ch8se/page-blog.php
+++ wp-content/themes/ch8se/page-blog.php
@@ -17,16 +17,6 @@ get_header(); ?>
 				  eu fugiat nulla pariatur.</p>
 				  <a href="">Lorem - posted an hour ago</a>
 			</div>
-			<div class="blog-post border">
-				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
-				<h3>Lorem ipsum dolor sit amet</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-				 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-				  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-				  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-				  eu fugiat nulla pariatur.</p>
-				  <a href="">Lorem - posted an hour ago</a>
-			</div>
 			<div class="blog-post">
 				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
 				<h3>Lorem ipsum dolor sit amet</h3>
@@ -47,7 +37,17 @@ get_header(); ?>
 				  eu fugiat nulla pariatur.</p>
 				  <a href="">Lorem - posted an hour ago</a>
 			</div>
-			<div class="blog-post border">
+			<div class="blog-post">
+				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				  eu fugiat nulla pariatur.</p>
+				  <a href="">Lorem - posted an hour ago</a>
+			</div>
+			<div class="blog-post">
 				<img src="http://www.ch8se.com/wp-content/uploads/2015/11/homeslider4.jpg">
 				<h3>Lorem ipsum dolor sit amet</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
