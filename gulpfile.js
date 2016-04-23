31f8ba2a7aa6d86772938e6103372459ef0dfaed
Radomir Perisic <radeperisic@yahoo.com>
2016-04-23 21:18:44 +0200

    gulp script update to use browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,69 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var livereload = require('gulp-livereload'); //requires LiveReload plugin in browser
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
-var concat = require("gulp-concat");
 var uglify = require('gulp-uglify');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
 var path = 'wp-content/themes/ch8se';
 
-gulp.task("javascript", function () {
-  return gulp.src(path + "/js/app.jsx")
-    .pipe(sourcemaps.init())
-    .pipe(plumber({errorHandler: notify.onError({title: 'JS compile error',message: "<%= error.message %>"})}))
-    .pipe(babel({
-            presets: ['es2015']
-        }))
-    .pipe(plumber.stop())
-    .pipe(concat("app.js"))
-    // .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(path + '/js/'));
+
+gulp.task('apply-prod-environment', function() {
+  process.env.NODE_ENV = 'production';
 });
 
-gulp.task("javascript-minify", function () {
-  return gulp.src(path + "/js/app.jsx")
-    .pipe(babel({
-            presets: ['es2015']
-        }))
-    .pipe(uglify())
-    .pipe(gulp.dest(path + '/js/'));
-});
+
 
 gulp.task('compile-less', function() {
   return gulp.src(path + '/less/style.less')
     .pipe(plumber({errorHandler: notify.onError({title: 'LESS compile error',message: "<%= error.message %>"})}))
     .pipe(less())
     .pipe(plumber.stop())
-    .pipe(gulp.dest(path));
-    // .pipe(connect.reload());
+    .pipe(gulp.dest(path))
+    .pipe(livereload());
 });
 
-// gulp.task('webserver', function() {
-//   connect.server({
-//     livereload: true,
-//     port: 3030
-//   });
-// });
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: "JS compile error",
+    message: "<%= error.message %>"
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
+
+gulp.task('browserify', function() {
+  return browserify(path + "/js/app.jsx", {debug: true})
+    .transform(babelify, {presets: ["es2015", "react"]})
+    .bundle().on('error', handleErrors)
+    .pipe(source("js/app.js"))
+    .pipe(gulp.dest(path))
+    .pipe(livereload());
+});
+
+gulp.task('browserify-uglify', function() {
+  return browserify(path + "/js/app.jsx", {debug: false})
+    .transform(babelify, {presets: ["es2015", "react"]})
+    .bundle().on('error', handleErrors)
+    .pipe(source("js/app.js"))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest(path))
+    .pipe(livereload());
+});
 
 
 gulp.task('watch', function() {
   gulp.watch(path + '/less/*.less' , ['compile-less']);
-  gulp.watch(path + '/js/app.jsx' , ['javascript']);
-  // livereload.listen(35729);
+  gulp.watch(path + '/js/app.jsx' , ['browserify']);
+  // gulp.watch('/*.html' , ['html']);
+  livereload.listen();
 });
 
-
-gulp.task('prod', ['compile-less', 'javascript-minify']);
-gulp.task('build', ['compile-less', 'javascript']);
-gulp.task('default', ['build',/* 'webserver',*/ 'watch']);
+gulp.task('build', ['browserify', 'compile-less']);
+gulp.task('production', ['apply-prod-environment', 'browserify-uglify', 'compile-less']);
+gulp.task('default', ['build', 'watch']);
